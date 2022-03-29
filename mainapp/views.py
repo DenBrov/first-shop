9a6c420930c23ae057a62a9296062453ac4cfbca
DenBrov <dbrovach@gmail.com>
2022-03-29 19:43:14 +0800

    created dumpdata

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 
 # вьюхи - функции - контроллеры
+from mainapp.models import Product, ProductCategory
+
 
 def index(request):
-    context = {'title': 'Shop'}
+    context = {
+        'title': 'Shop',
+    }
     return render(request, 'mainapp/index.html', context)
 
 def products(request):
-    context = {'title': 'Shop - Каталог'}
+    context = {
+            'title': 'Shop - Каталог',
+            'products': Product.objects.all(),
+            'categories': ProductCategory.objects.all(),
+    }
     return render(request, 'mainapp/products.html', context)
 
 def base(request):
