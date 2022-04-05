622dd007ad23215cc3f245e7a9017d9c3d1d461c
DenBrov <dbrovach@gmail.com>
2022-04-05 21:59:01 +0800

    prepared profile.html

--- authapp/views.py
+++ authapp/views.py
@@ -32,6 +32,9 @@ def register(request):
     context = {'form': form}
     return render(request, 'authapp/register.html', context)
 
+def profile(request):
+    return render(request, 'authapp/profile.html')
+
 def logout(request):
     auth.logout(request)
     return HttpResponseRedirect(reverse('index'))
