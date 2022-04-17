7823710d8425cf75ab21af239010ab09cd18b475
DenBrov <dbrovach@gmail.com>
2022-04-17 22:57:39 +0800

    added user_passes_test

--- adminapp/views.py
+++ adminapp/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import user_passes_test
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
@@ -5,17 +6,18 @@ from django.urls import reverse
 from authapp.models import User
 from adminapp.forms import UserAdminRegistrationForm, UserAdminProfileForm
 
+@user_passes_test(lambda u: u.is_superuser)
 def index(request):
     return render(request, 'adminapp/index.html')
 
 
-#READ
+# READ
 def admin_users(request):
     context = {'users': User.objects.all()}
     return render(request, 'adminapp/admin-users-read.html', context)
 
 
-#CREATE
+# CREATE
 def admin_users_create(request):
     if request.method == 'POST':
         form = UserAdminRegistrationForm(data=request.POST, files=request.FILES)
@@ -28,7 +30,7 @@ def admin_users_create(request):
     return render(request, 'adminapp/admin-users-create.html', context)
 
 
-#UPDATE
+# UPDATE
 def admin_users_update(request, user_id):
     user = User.objects.get(id=user_id)
     if request.method == 'POST':
@@ -42,7 +44,7 @@ def admin_users_update(request, user_id):
     return render(request, 'adminapp/admin-users-update-delete.html', context)
 
 
-#DELETE
+# DELETE
 def admin_users_delete(request, user_id):
     user = User.objects.get(id=user_id)
     user.is_active = False
@@ -50,10 +52,9 @@ def admin_users_delete(request, user_id):
     return HttpResponseRedirect(reverse('admin_staff:admin_users'))
 
 
-#RESTORE
+# RESTORE
 def admin_users_restore(request, user_id):
     user = User.objects.get(id=user_id)
     user.is_active = True
     user.save()
     return HttpResponseRedirect(reverse('admin_staff:admin_users'))
-
